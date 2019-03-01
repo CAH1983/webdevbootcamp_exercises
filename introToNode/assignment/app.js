748172c5594a4bc28c9d8f7c1d9789827f1c1a1f
CAH1983 <carolineho1983@yahoo.fr>
2019-03-01 18:34:29 +0000

    added instructor's solution

--- introToNode/assignment/app.js
+++ introToNode/assignment/app.js
@@ -5,19 +5,20 @@ app.get('/', function(req, res) {
     res.send('Hi there, welcome to my assignement!');
 });
 
-
+// =========== My solution =================
 app.get('/speak/:animal', function(req, res) {
         let animal = req.params.animal;
 
     if (animal === 'pig'){
         console.log(animal);
-        res.send("the pig says 'Oink'");
+        res.send("the " + animal + " says 'Oink'");
     }
     if (animal === 'dog'){
-        res.send("the dog says 'whoof'");
+        res.send("the " + animal + " says 'whoof'");
+    } else {
+        res.send("the " + animal + " says whatever");
     }
 });
-
 
 
 app.get('/repeat/:word/:numTimes', function(req, res) {
@@ -25,11 +26,39 @@ app.get('/repeat/:word/:numTimes', function(req, res) {
     let howManyTimes = parseInt(req.params.numTimes);
     console.log(wordToRepeat, howManyTimes);
     res.send((wordToRepeat+ ' ').repeat(howManyTimes));
-
 });
 
+app.get('*', function(req, res) {
+    res.send('⛔️Error 404');
+})
+// ========== teacher's solutions ===============
+// app.get('/speak/:animal', function(req, res) {
+//     var sounds = {
+//         pig: 'oink',
+//         cow: 'Moo',
+//         dog: 'woof',
+//         cat: 'I hate you human',
+//         goldfish: '...'
+//     }
+//     var animal = req.params.animal.toLowerCase();
+//     var sound = sounds[animal];
+//     res.send("The " + animal + " says '" + sound + "'");
+// });
 
+// app.get("/repeat/:message/:times", function(req, res) {
+//     var message = req.params.message;
+//     var times = Number(req.params.times);
+//     var result = "";
 
+//     for(var i=0; i<times; i++) {
+//         result += message + ' ';
+//     }
+//     res.send(result);
+// })
+
+// app.get('*', function(req, res) {
+//     res.send('Sorry, page not found.. what are you doing with your life?')
+// });
 
 // start server
 app.listen(3000, function() {
