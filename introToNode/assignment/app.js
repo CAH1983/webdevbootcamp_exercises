acc96dbb16977360a5a6f9039b5910c14f6cf7c9
CAH1983 <carolineho1983@yahoo.fr>
2019-03-01 18:01:06 +0000

    solved both challenges

--- introToNode/assignment/app.js
+++ introToNode/assignment/app.js
@@ -5,39 +5,19 @@ app.get('/', function(req, res) {
     res.send('Hi there, welcome to my assignement!');
 });
 
-// app.get(url, function(req, res) {
-//     switch(url) {
-//         case '/speak/pig':
-//         res.send("The pig says 'Oink'")
-//         break;
 
-//         case '/speak/cow':
-//         res.send("The cow says 'Moo'")
-//         break;
-//     }
-// });
+app.get('/speak/:animal', function(req, res) {
+        let animal = req.params.animal;
 
-// app.get('/speak/:animal', function(req, res) {
-//     if (req.params.animal === 'pig'){
-//         console.log(params.animal);
-//         res.send("the pig says 'Oink'");
-//     }
-//     if (req.params.animal === 'dog'){
-//         res.send("the dog says 'whoof'");
-//     }
-// });
-
-app.get('/speak/pig', function(req, res) {
-    res.send("the pig says 'Oink'");
+    if (animal === 'pig'){
+        console.log(animal);
+        res.send("the pig says 'Oink'");
+    }
+    if (animal === 'dog'){
+        res.send("the dog says 'whoof'");
+    }
 });
 
-app.get('/speak/dog', function(req, res) {
-    res.send("the dog says 'whoof'");
-});
-
-app.get('/speak/cat', function(req, res) {
-    res.send("the cat says 'Mooh'");
-});
 
 
 app.get('/repeat/:word/:numTimes', function(req, res) {
