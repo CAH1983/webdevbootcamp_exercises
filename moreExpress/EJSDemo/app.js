b231d06f799d59a2350d52bb6cc7a69094ea1224
CAH1983 <carolineho1983@yahoo.fr>
2019-03-01 23:00:45 +0000

    creating posts route

--- moreExpress/EJSDemo/app.js
+++ moreExpress/EJSDemo/app.js
@@ -12,7 +12,18 @@ app.get('/drinkabeerwith/:character', function(req,res){
     res.render('beer.ejs', {charac});
 });
 
-
+app.get('/posts', function(req, res) {
+    var posts = [
+        {title: 'Make beers great again', author: 'Donald Trump'},
+        {title: 'I prefer vodka sorry', author: 'Boris Elstine'},
+        {title: 'Any alcoholic drinks would do me', author: 'Theresa May'},
+        {title: 'Vodka', author: 'Vladimir Poutine'},
+        {title: 'OktoberFest beers', author: 'Angela Merkel'},
+        {title: 'Rhum', author: 'Che Guevara'},
+        {title: 'Only champagne with Krusty', author: 'Kim Kardashian'},
+    ];
+    res.render('posts.ejs', {posts});
+})
 
 app.listen(3000, function(){
     console.log('server is listening');
