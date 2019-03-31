a814fe4a97b55a7f1afbf2ea34dd55b83608d64f
CAH1983 <carolineho1983@yahoo.fr>
2019-03-31 23:19:00 +0100

    stack examples

--- advanced_web_dev/async_foundations.js
+++ advanced_web_dev/async_foundations.js
@@ -9,7 +9,7 @@ function double(arr) {
 
 double(arr);
 
-// refactor with forEach 
+// refactored with forEach 
 function double() {
     var arr = [1, 2, 3, 4, 5, 6];
     arr.forEach(function(number) {
@@ -18,3 +18,36 @@ function double() {
 }
 
 double();
+
+// findIndex returns the index of the 1st elem in the array for which the callbacl returns a truthy value. -1 is returned if the callback never returns a truthy value
+// syntax
+// function findIndex(array, callback) {
+//     code to run
+// }
+
+
+var arr = [3, 4, 6, 2, 1];
+findIndex(arr, function(num, index, array) {
+    return num === 6;
+});
+// will return index of 2
+
+var arr = [5, 11.13, 8, 6, 7];
+findIndex(arr, function(num, index, array) {
+    return num%2 === 0;
+});
+
+// stack example
+function upperCaseFirst(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
+
+function upperCaseWords(sentence) {
+    var words = sentence.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = upperCaseFirst(words[i]);        
+    }
+    return words.join(' ');
+}
+
+upperCaseWords('lowercase words');
