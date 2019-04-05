ed126f827e2d6121f32a585c6daada12ca8e29b7
CAH1983 <carolineho1983@yahoo.fr>
2019-04-05 15:19:59 +0100

    XML request dog images exercise

--- advanced_web_dev/xmlrequest.js
+++ advanced_web_dev/xmlrequest.js
@@ -1,11 +1,46 @@
-var XHR = new XMLHttpRequest();
+// var XHR = new XMLHttpRequest();
 
-XHR.open('GET', 'https://api.github.com/zen');
-XHR.onreadystatechange = function() {
-    if(XHR.readyState == 4) {
-        console.log(XHR.responseText)
+// XHR.open('GET', 'https://api.github.com/zen');
+// XHR.onreadystatechange = function() {
+//     if(XHR.readyState == 4) {
+//         if(XHR.status == 200) {
+//         console.log(XHR.responseText);
+//         } else {
+//         console.log('there was a problem');
+//         }
+//     }
+// } 
+
+// XHR.open('GET', 'https://api.github.com/zen');
+// XHR.send();
+
+var btn = document.querySelector("#btn");
+var img = document.querySelector('#photo');
+
+btn.addEventListener('click', function() {
+    // make the request
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function() {
+        if(XHR.readyState == 4 && XHR.status == 200) {
+            console.log(XHR.responseText);
+            var url = JSON.parse(XHR.responseText).message;
+            img.src = url; 
+        }
     }
-}
 
-XHR.open('GET', 'https://api.github.com/zen');
-XHR.send();
+    XHR.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    XHR.send();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
