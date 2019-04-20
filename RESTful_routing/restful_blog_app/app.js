f681339ac2c0511120a50557954ce5135d89f8f2
CAH1983 <carolineho1983@yahoo.fr>
2019-04-20 21:14:10 +0100

    Restful routing exercise

---
+++ RESTful_routing/restful_blog_app/app.js
@@ -0,0 +1,39 @@
+var bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
+express = require('express'),
+app = express();
+
+// APP CONFIG
+mongoose.connect('mongodb://localhost/restful_blog_app', {
+    useNewUrlParser: true
+});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE /MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date, 
+        default: Date.now //default is for set a value as default
+    }
+})
+
+var Blog = mongoose.model('blog', blogSchema);
+
+Blog.create({
+    title: 'Test blog',
+    image: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1560&q=80',
+    body: 'this is a blog post',
+
+});
+// RESTful ROUTES
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('SERVER IS RUNNING!! *_*');
+    
+})
